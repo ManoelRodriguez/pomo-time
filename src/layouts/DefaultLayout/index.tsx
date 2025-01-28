16a8c5facdc0ac49d9d64d1e0b549d9390908e63
Manoel Rodriguez <manoel.rodriguez98@gmail.com>
2025-01-28 16:18:52 -0300

    navbar completo

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
+import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
     return (
-        <div>
+        <LayoutContainer>
             <Header />
             <Outlet /> {/* //Usado para posicionar o restante da página que não é "padrão" */}
-        </div>
+        </LayoutContainer>
     )
 }
