9cc0c0490e9304ee1335bc1ed3b14b5ea3bb6980
Manoel Rodriguez <manoel.rodriguez98@gmail.com>
2025-02-04 16:25:21 -0300

    Renderizando tempo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form"
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useState } from "react";
 
 import {
     CountdownContainer,
@@ -13,6 +14,7 @@ import {
     TaksInput
 }
     from "./styles";
+
 
 const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, "Por favor, insira um nome para a tarefa"),
@@ -26,7 +28,17 @@ interface NewCycleFormData {
     minutesAmount: number
 }
 
+interface Cycle {
+    id: string,
+    task: string,
+    minutesAmount: number,
+}
+
 export function Home() {
+
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
     const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
@@ -36,11 +48,32 @@ export function Home() {
         }
     })
 
+    const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+    const minutesAmount = Math.floor(currentSeconds / 60)
+    const secondsAmount = currentSeconds % 60
+
+    const minutes = String(minutesAmount).padStart(2, "0") // Adiciona um 0 a esquerda se o número tiver apenas 1 dígito
+    const seconds = String(secondsAmount).padStart(2, "0") // Adiciona um 0 a esquerda se o número tiver apenas 1 dígito
+
     const task = watch("task")
     const isSubmitDisabled = !task
 
     function handleCreateNewCycle(data: NewCycleFormData) {
-        console.log(data)
+
+        const id = String(new Date().getTime())
+
+        const newCycle: Cycle = {
+            id,  // Gera um id único -  transforma o timestamp em milessegundos em string
+            task: data.task,
+            minutesAmount: data.minutesAmount
+        }
+
+        setCycles([...cycles, newCycle]) // (state) => [(...state + newCycle)]
+        setActiveCycleId(id)
         reset()
     }
 
@@ -79,11 +112,11 @@ export function Home() {
                 </ FormContainer>
 
                 <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountdownContainer>
 
                 <StartCountdownButton disabled={isSubmitDisabled} type="submit">
